cffebb8dcc9daaddc45113d1904dbf5a6ebd08b3
solda <solda@khu.ac.kr>
2021-04-27 22:51:56 +0900

    updated

--- 5/empty-blueprint/src/index.js
+++ 5/empty-blueprint/src/index.js
@@ -4,56 +4,77 @@
 
 const taskInput = document.querySelector("input");
 const pendingList = document.querySelector(".pendingList");
+const finishedList = document.querySelector(".finishedList");
 
-const TASK_KEY = "task";
+const P_TASK_KEY = "PENDING";
+const F_TASK_KEY = "FINISHED";
 
-let tasks = [];
+let p_tasks = [];
+let f_tasks = [];
 
 function deleteTask(event) {
     const btn = event.target;
     const li = btn.parentNode;
     pendingList.removeChild(li);
 
-    console.log(tasks.id);
-    console.log(li.id);
-    const clean = tasks.filter(function (task) {
-        return tasks.id !== parseInt(li.id);
+    const clean = p_tasks.filter(function (task) {
+        return task.id !== parseInt(li.id); // false > filter
     });
 
-    console.log(clean);
-    tasks = clean;
+    p_tasks.push(clean);
 
     saveTask();
 }
 
+function finishTask(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    finishedList.appendChild(li);
+
+    const clean = f_tasks.filter(function (task) {
+        return task.id === parseInt(li.id);
+    });
+
+    const span = document.querySelector("span");
+    const text = span.innerText;
+    console.log(text);
+    //    f_tasks = li;
+
+    localStorage.setItem(F_TASK_KEY, JSON.stringify(f_tasks)); //
+}
+
 function saveTask() {
-    localStorage.setItem(TASK_KEY, JSON.stringify(tasks));
+    localStorage.setItem(P_TASK_KEY, JSON.stringify(p_tasks));
 }
 
 function paint(text) {
     const li = document.createElement("li");
     const deleteBtn = document.createElement("button");
+    const finishedBtn = document.createElement("button");
     const span = document.createElement("span");
-    const newId = tasks.length + 1;
+    //const newId = tasks.length + 1;
+    const newId = Date.now();
 
     deleteBtn.innerHTML = "❌";
     deleteBtn.addEventListener("click", deleteTask);
 
-    //finishedBtn.innerHTML = "✅";
-    //finishedBtn.addEventListener("click", finishTask);
+    finishedBtn.innerHTML = "✅";
+    finishedBtn.addEventListener("click", finishTask);
 
     span.innerText = text;
 
+    li.id = newId;
     li.appendChild(span);
     li.appendChild(deleteBtn);
-    //li.appendChild(finishedBtn);
+    li.appendChild(finishedBtn);
+
     pendingList.appendChild(li);
 
     const taskObject = {
         id: newId,
         text: text,
     };
-    tasks.push(taskObject);
+    p_tasks.push(taskObject);
 
     saveTask();
 }
@@ -67,19 +88,25 @@ function handleChange(event) {
 }
 
 function loadTask() {
-    const loadTasks = localStorage.getItem(TASK_KEY);
+    const loadTasks1 = localStorage.getItem(P_TASK_KEY);
 
-    if (loadTasks !== null) {
-        const parseTasks = JSON.parse(loadTasks);
+    if (loadTasks1 !== null) {
+        const parseTasks = JSON.parse(loadTasks1);
         parseTasks.forEach(function (t) {
             paint(t.text);
         });
     }
 }
-
+/*
 const init = () => {
     loadTask();
     taskInput.addEventListener("change", handleChange);
 };
+*/
+
+function init() {
+    loadTask();
+    taskInput.addEventListener("change", handleChange);
+}
 
 init();
