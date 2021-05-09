307cc663abd24d201213d7b51613a35fe22e4f7b
DaSol Kim <solda@khu.ac.kr>
2021-05-09 22:49:11 +0900

    last updated

--- momonton-master/momonton-master/js/clock.js
+++ momonton-master/momonton-master/js/clock.js
@@ -1,20 +1,21 @@
 const clock = document.querySelector(".js-clock .clock__text");
 
 function getTime() {
-  const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const time = `${hours < 10 ? `0${hours}` : hours}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  }`;
-  clock.innerHTML = time;
-  return;
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    clock.innerHTML = `${hours < 10 ? `0${hours}` : `${hours}`}:${minutes < 10 ? `0${minutes}` : minutes}:${
+        seconds > 9 ? `${seconds}` : `0${seconds}`
+    }`;
+    return;
 }
 
 function init() {
-  getTime();
-  setInterval(getTime, 1000);
-  return;
+    getTime();
+    setInterval(getTime, 1000);
+    return;
 }
 
 init();
--- momonton-master/momonton-master/js/toDo.js
+++ momonton-master/momonton-master/js/toDo.js
@@ -1,73 +1,75 @@
-const form = document.querySelector(".js-to-do"),
-  input = document.querySelector(".js-add-to-do"),
-  list = document.querySelector(".js-list");
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
 
 let toDos = [];
 
-function persistToDos() {
-  const stringToDo = JSON.stringify(toDos);
-  localStorage.setItem("toDos", stringToDo);
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    //console.log(event.target.parentNode);
+    const cleanToDos = toDos.filter(function (toDo) {
+        return toDo.id !== parseInt(li.id);
+    }); // ≒forEach(function)
+    // filter:array의 모든 item을 통해 함수 실행하고 true인 itme만 가지고 새로운 array 생성
+    toDos = cleanToDos; // let
+    saveToDos();
 }
 
-function saveToDo(text) {
-  const toDoObject = {
-    id: toDos.length + 1,
-    value: text
-  };
-  toDos.push(toDoObject);
-  persistToDos();
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); // LS에 JS의 data 저장할 수 없고 string만 가능
 }
 
-function handleDelete(event) {
-  const target = event.target;
-  const li = target.parentElement;
-  const ul = li.parentElement;
-  const toDoId = li.id;
-  ul.removeChild(li);
-  toDos = toDos.filter(function(toDo) {
-    return toDo.id !== parseInt(toDoId);
-  });
-  persistToDos();
+function paintToDo(text) {
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+
+    delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
+    span.innerText = text;
+
+    li.id = newId; // <li id="1">
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+
+    const toDoObj = {
+        text: text,
+        id: newId,
+    };
+    toDos.push(toDoObj); // 배열 추가
+
+    saveToDos();
 }
 
-function addToDo(text) {
-  const toDo = document.createElement("li");
-  toDo.className = "toDo";
-  toDo.id = toDos.length + 1;
-  const deleteBtn = document.createElement("span");
-  deleteBtn.innerHTML = "❌";
-  deleteBtn.className = "toDo__button";
-  deleteBtn.addEventListener("click", handleDelete);
-  const label = document.createElement("label");
-  label.innerHTML = text;
-  toDo.appendChild(deleteBtn);
-  toDo.appendChild(label);
-  list.appendChild(toDo);
-  saveToDo(text);
-}
+function handleSubmit(event) {
+    event.preventDefault(); // event 금지 방지
 
-function onSubmit(event) {
-  event.preventDefault();
-  const value = input.value;
-  input.value = "";
-  addToDo(value);
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = ""; // input 다시 공백으로 만듦
 }
 
 function loadToDos() {
-  const loadedToDos = localStorage.getItem("toDos");
-  if (loadedToDos !== null) {
-    const parsedToDos = JSON.parse(loadedToDos);
-    parsedToDos.forEach(function(toDo) {
-      addToDo(toDo.value);
-    });
-  }
-  return;
+    const loadedToDos = localStorage.getItem(TODOS_LS); // ???
+
+    if (loadedToDos !== null) {
+        const parseToDos = JSON.parse(loadedToDos); // object
+        parseToDos.forEach(function (toDo) {
+            // forEach:array function
+            paintToDo(toDo.text);
+        });
+    }
 }
 
 function init() {
-  loadToDos();
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
 }
-
-form.addEventListener("submit", onSubmit);
 
 init();
